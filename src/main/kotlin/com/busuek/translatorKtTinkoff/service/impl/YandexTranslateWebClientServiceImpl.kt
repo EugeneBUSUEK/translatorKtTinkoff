6ae1f48e5fd8e4aa2bb35a8a43f0ec267d680b7a
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-03-24 20:16:56 +0300

    -

--- src/main/kotlin/com/busuek/translatorKtTinkoff/service/impl/YandexTranslateWebClientServiceImpl.kt
+++ src/main/kotlin/com/busuek/translatorKtTinkoff/service/impl/YandexTranslateWebClientServiceImpl.kt
@@ -3,6 +3,7 @@ package com.busuek.translatorKtTinkoff.service.impl
 import com.busuek.translatorKtTinkoff.dto.request.YandexTranslateDTO
 import com.busuek.translatorKtTinkoff.dto.response.YandexTranslateResultDTO
 import com.busuek.translatorKtTinkoff.service.YandexTranslateWebClientService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Service
@@ -11,12 +12,14 @@ import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 
 @Service
-class YandexTranslateWebClientServiceImpl: YandexTranslateWebClientService {
+class YandexTranslateWebClientServiceImpl(
+    @Value("\${yandex.api.token}")
+    private val token: String,
 
+    @Value("\${yandex.folder.id}")
+    private val folderId: String
+) : YandexTranslateWebClientService {
 
-    val token: String = "t1.9euelZrIiZGbz5aMx8zJzpLOx4nIlO3rnpWazY6LjJuXicqUmoqYyc-Tkpnl8_dwfwhf-e8WPV98_t3z9zAuBl_57xY9X3z-.Qlf1JukfWUirbsfafWzZlJ5StRPUuCd7NtroKHgC_fWESruOEf8dFB_CCVUJZr7tEMw7ierc5yGsN4Ytl52KDQ"
-
-    val folderId: String = "b1gpmmgv8etd5678mt5q"
 
     override fun getTranslatedWords(targetLanguageCode: String, sourceWords: List<String>): List<String> {
         val template = YandexTranslateDTO(folderId, sourceWords, targetLanguageCode)
